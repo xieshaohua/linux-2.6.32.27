b71c6e129e7b9119ace0e97d053ff594e73978c1
xieshaohua <xieshaohua_07@126.com>
2017-07-15 21:41:24 +0800

    YAFFS2: suppress prompts "Partially written block xxx detected" when booting up

--- fs/yaffs2/yaffs_guts.c
+++ fs/yaffs2/yaffs_guts.c
@@ -6434,7 +6434,7 @@ static int yaffs_ScanBackwards(yaffs_Device *dev)
 							 * the current allocation block.
 							 */
 
-							 T(YAFFS_TRACE_ALWAYS,
+							 T(YAFFS_TRACE_SCAN,
 							 (TSTR("Partially written block %d detected" TENDSTR),
 							 blk));
 						}
